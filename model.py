5086282ee17bc9415ac511b27991ed08d07873ab
Obi Ohagwu <obiohagwu8@gmail.com>
2022-08-23 18:13:12 -0400

    got positioning

--- model.py
+++ model.py
@@ -18,24 +18,31 @@ cv2.namedWindow('image', cv2.WINDOW_NORMAL)
 W = 1920//2
 H= 1080//2
 
-window = sdl2.ext.Window("SLAM-FEED", size=(W, H))
+window = sdl2.ext.Window("SLAM-FEED", size=(W, H), position=(-500,500))
 window.show() 
 
 def process_frame(img):
     img = cv2.resize(img,(W,H))
     log_events = sdl2.ext.get_events()
-    cv2.imshow('live-video-feed', img)
-    #cv2.waitKey(0)
-    print(img.shape)
-    #print(img)
+    for event in log_events:
+        if event.type == sdl2.SDL_QUIT:
+            exit(0)
+    print(dir(window))
+    surf = sdl2.ext.pixels2d(window.get_surface())
+    surf[:] = img.swapaxes(0,1)[:, :, 0]
     window.refresh()
+    #cv2.imshow('FEED', img)
+    #cv2.waitKey(0)
+    #print(img.shape)
+    #print(img)
+    
 
 
 def test():
     print("Testing")
 
 if __name__ == "__main__":
-    cap = cv2.VideoCapture("unlabeled/7.hevc")
+    cap = cv2.VideoCapture("unlabeled/6.hevc.mp4")
     
     i=0
     while cap.isOpened():
